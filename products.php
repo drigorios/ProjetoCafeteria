85a7c597157d128c487393c0146f203b22f11fb1
fengcastelo <ianfcastelo@gmail.com>
2024-02-07 20:45:15 -0300

    Error Updating

---
+++ products.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+    <link rel="stylesheet" href="navbar.css">
+</head>
+<body>
+        <div class="navbar show-menu">
+            <div class="header ">
+                <h1 class="logo"><a class="logotype" href="index.html">Brew <span>Haven.</span></a></h1>
+                <nav>
+                    <ul>
+                        <li> <a href="index.html">Home</a></li>
+                        <li><a href="products.php">Produtos</a></li>
+                        <li>Sobre</li>
+                        <li>Contato</li>
+                        <li><a href="login.html">Login</a></li>
+                    </ul>
+                </nav>
+                <div class="icons">
+                    <img src="imagens/cart.png">
+                    <img class="menu" src="imagens/menu.png" alt="">
+                </div>
+            </div>
+        </div>
+</body>
+</html>
